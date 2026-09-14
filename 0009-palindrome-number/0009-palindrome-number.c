7a8160922f268103cc98f35dcf58bdc20c66aee9
Aryush Sheelavant <aryushsheelavant@gmail.com>
2026-09-14 17:59:21 +0530

    Time: 1 ms (51.73%), Space: 8.9 MB (21.88%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    
+    if (x < 0) {
+        return false;
+    }
+
+    long long original = x;
+    long long rev = 0;
+
+    while (x > 0) {
+        int rem = x % 10;
+        rev = rev * 10 + rem;
+        x = x / 10;
+    }
+
+    return original == rev;
+}
